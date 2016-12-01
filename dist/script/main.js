ebb81487de8fada96eb2fe758e0495074de42aab
MrHidalgo <Luckyhidalgo@gmail.com>
2016-12-01 18:23:59 +0200

    Add discover, footer, maps, partner, price, program, speaker, sponsor, subscribe page with all opt - !adap

--- dist/script/main.js
+++ dist/script/main.js
@@ -1,3 +1,31 @@
-'use strict';
+function initialize() {
+    var myOptions = {
+        zoom: 14,
+        scrollwheel: false,
+        center: new google.maps.LatLng(50.440592, 30.49163599999997),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);
+    marker = new google.maps.Marker({
+        map: map,
+        position: new google.maps.LatLng(50.440592, 30.49163599999997)
+    });
+    infowindow = new google.maps.InfoWindow({
+        content: "<b>KDS</b><br/>Vokzalna St, 1, Kyiv, <br/>02000 Kiev"
+    });
+    google.maps.event.addListener(marker, "click", function() {
+        infowindow.open(map, marker);
+    });
+    var center = map.getCenter();
+
+    google.maps.event.addDomListener(window, 'resize', function() {
+        map.setCenter(center);
+    });
+    infowindow.open(map, marker);
+}
+if($("#gmap_canvas").length > 0) {
+    google.maps.event.addDomListener(window, 'load', initialize);
+}
+
 
 $(document).ready(function(){});
---
+++ task.md
@@ -0,0 +1,7 @@
+1. Gradient block
+2. Smart Filter
+3. Timer
+4. Animation for news block
+5. Animation load block
+5. Optimization img
+6. 
